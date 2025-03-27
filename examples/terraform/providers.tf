8ab67c3904a80113042863a8c5a5a0a840e70a28
Luciano Sampaio <lsampaioweb@gmail.com>
2025-03-26 21:21:03 -0300

    feat: Add null provider to providers.tf with version 3.2.3

--- examples/terraform/providers.tf
+++ examples/terraform/providers.tf
@@ -5,5 +5,10 @@ terraform {
       source  = "hashicorp/local"
       version = "2.5.2"
     }
+    null = {
+      # https://registry.terraform.io/providers/hashicorp/null/latest
+      source  = "hashicorp/null"
+      version = "3.2.3"
+    }
   }
 }
