49663c90f021e2ac57cd604ad0213d52c5a6fd1c
Luciano Sampaio <lsampaioweb@gmail.com>
2025-03-30 18:54:20 -0300

    fix: Correct output values in output.tf for dynamic inventory attributes

--- output.tf
+++ output.tf
@@ -1,24 +1,24 @@
 output "content" {
-  value       = local_file.dynamic_inventory.content
   description = "The content of the created file."
+  value       = local_file.dynamic_inventory.content
 }
 
 output "directory_permission" {
-  value       = local_file.dynamic_inventory.directory_permission
   description = "The permission of the created directory."
+  value       = local_file.dynamic_inventory.directory_permission
 }
 
 output "file_permission" {
-  value       = local_file.dynamic_inventory.file_permission
   description = "The permission of the created file."
+  value       = local_file.dynamic_inventory.file_permission
 }
 
 output "filename" {
-  value       = local_file.dynamic_inventory.filename
   description = "The path and name of the file."
+  value       = local_file.dynamic_inventory.filename
 }
 
 output "id" {
-  value       = local_file.dynamic_inventory.id
   description = "The id of the file."
+  value       = local_file.dynamic_inventory.id
 }
